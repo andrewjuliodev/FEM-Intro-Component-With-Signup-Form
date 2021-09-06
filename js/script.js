76a440f51ee4eb861500b0929b56cb27a5c0a486
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-06 02:02:03 +0200

    Finished JS Validation Logic and Style Manipulation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 const inputText = document.getElementsByClassName("row");
 const submit = document.querySelector(".submit");
-const card = document.querySelector(".card");
-let cardHeight = (document.querySelector(".card").style.height = 475);
-let height;
+const email = document.querySelector(".email");
+
+const emailValid = function (email) {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+};
+
+//break at i = 2 continue at i =4, write conditional for i = 3 separately
 
 submit.addEventListener("click", function () {
   for (let i = 1; i < inputText.length + 1; i++) {
-    if (document.querySelector(`.row${i}`).childNodes[1].value === "") {
+    if (
+      document.querySelector(".row3").childNodes[1].value === "" ||
+      !emailValid(email.value)
+    ) {
+      email.placeholder = "example@email.com";
+      email.style.border = "1px solid hsl(0, 100%, 74%)";
       document.querySelector(`.error${i}`).style.display = "block";
       document.querySelector(`.input${i}`).style.background =
         "url('/images/icon-error.svg')";
@@ -14,11 +23,28 @@ submit.addEventListener("click", function () {
       document.querySelector(`.input${i}`).style.backgroundPosition =
         "410px, 0px";
       document.querySelector(`.input${i}`).style.backgroundSize = "5%";
-      document.querySelector(".card").style.height = "550px";
+      document.querySelector(".card").style.height = "540px";
+      if (i === 3) {
+        continue;
+      }
+    }
+
+    if (document.querySelector(`.row${i}`).childNodes[1].value === "") {
+      document.querySelector(`.input${i}`).style.border =
+        "1px solid hsl(0, 100%, 74%)";
+      document.querySelector(`.error${i}`).style.display = "block";
+      document.querySelector(`.input${i}`).style.background =
+        "url('/images/icon-error.svg')";
+      document.querySelector(`.input${i}`).style.backgroundRepeat = "no-repeat";
+      document.querySelector(`.input${i}`).style.backgroundPosition =
+        "410px, 0px";
+      document.querySelector(`.input${i}`).style.backgroundSize = "5%";
+      document.querySelector(".card").style.height = "540px";
     } else {
       document.querySelector(`.error${i}`).style.display = "none";
+      document.querySelector(`.input${i}`).style.background = "none";
+      document.querySelector(`.input${i}`).style.border =
+        "1px solid hsl(246, 25%, 77%)";
     }
   }
 });
-
-//end result is 47515px
